437c78c2f5ce2a3bb5291a4311c2dba3a5faa7d5
irfanveb <irfan03074146341@gmail.com>
2026-06-23 10:49:03 +0500

    Add data structures example in Urdu

---
+++ data_structures.py
@@ -0,0 +1,16 @@
+# 1. پسندیدہ ایپس کی لسٹ (List)
+my_apps = ["VS Code", "GitHub", "Pydroid 3"]
+
+# 2. اپنے کورس کی معلومات کی ڈکشنری (Dictionary)
+my_course = {
+    "language": "Python",
+    "days_completed": 2,
+    "status": "Learning"
+}
+
+# 3. ان دونوں کو اسکرین پر پرنٹ (Show) کرنا
+print("Meri pasandida apps:", my_apps)
+print("Kouras ki maloomat:", my_course)
+
+# 4. ڈکشنری سے کوئی ایک خاص چیز پرنٹ کرنا
+print("Main abhi seekh raha hoon:", my_course["language"])
