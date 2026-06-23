b44e923989432d745964a17c3f2cc13bd8e19f72
irfanveb <irfan03074146341@gmail.com>
2026-06-23 10:55:22 +0500

    Create add_numbers function to sum two numbers
    Added a function to sum two numbers and print the result.

---
+++ functions.py
@@ -0,0 +1,8 @@
+# 1. فنکشن بنانا جو دو نمبرز کو جمع کرے گا
+def add_numbers(num1, num2):
+    total = num1 + num2
+    print("Dono numbers ka jor (Sum) hai:", total)
+
+# 2. فنکشن کو الگ الگ نمبرز دے کر چلانا
+add_numbers(10, 20)
+add_numbers(150, 350)
